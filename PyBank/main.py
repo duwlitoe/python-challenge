708f0e1fa88f02f05cae0d6203db521598549b67
Duwli Toe <Duwli.toe@gmail.com>
2019-08-28 06:59:27 -0400

    Created new file

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,6 @@ import os
 # Module for reading CSV files
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('..','Resources', 'budget_data.csv')
 
 # Add total number of months of data included
