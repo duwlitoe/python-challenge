59c99a9d94191856cc9498f2ab179520a05ab188
Duwli Toe <Duwli.toe@gmail.com>
2019-08-28 21:30:18 -0400

    modified Pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,21 @@ import os
 # Module for reading CSV files
 import csv
 
-csvpath = os.path.join('..','Resources', 'budget_data.csv')
+# Path to collect data from the Resources folder
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+# Open and read csv
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+
+
+
 
 # Add total number of months of data included
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+
+
+    
+
